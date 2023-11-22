74bc21cb05bc03e5cbb8d9c0cedcc99156b168f9
Marvel Pangondian <marvel.pangondian01@gmail.com>
2023-11-22 14:36:33 +0700

    feat: modify token

--- tag2_symbol.py
+++ tag2_symbol.py
@@ -98,3 +98,87 @@ def remove_content_tag(html_content):
 
 # # Print or save the modified HTML code
 # print(modified_html)
+
+
+from bs4 import BeautifulSoup
+import re
+
+def remove_content_except_head(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Find all tags except 'head' and remove their contents
+    for tag in soup.find_all(lambda t: t.name != 'head'):
+        tag.string = ''
+
+    cleaned_html = str(soup)
+    print(cleaned_html)
+    return cleaned_html
+
+def is_html_tag(tag):
+    # Define a regular expression pattern for HTML tags
+    pattern = re.compile(r'^<([a-zA-Z][^\s>]*)\s*[^>]*>$|^</([a-zA-Z][^\s>]*)\s*>$')
+    match = pattern.match(tag)
+    return bool(match)
+    
+    # Use the pattern to match the tag
+    match = pattern.match(tag)
+    
+    # Return True if it's a match, otherwise False
+    return bool(match)
+
+def is_void_element(tag):
+    # Define a regular expression pattern for void HTML elements
+    pattern = re.compile(r'^<([a-zA-Z][^\s>]*)\s*[^>]*(?<!/)>$')
+    
+    # Use the pattern to match the tag
+    match = pattern.match(tag)
+    
+    # Return True if it's a match, otherwise False
+    return bool(match)
+
+def remove_content(html_arr):
+    not_allowed_text_in_tags = ["<head>","<html>","<body>","<div>","<table>","<link .*?>",]
+    parent_tags = ["<body>","<head>","<html>",]
+
+
+    initial_tag = ''
+    parent_tag = ""
+    for i in range(len(html_arr)):
+        if is_html_tag(html_arr[i]):
+            initial_tag = html_arr[i]
+        else:
+            if initial_tag in not_allowed_text_in_tags or initial_tag[0:2] =='</'  :
+                html_arr[i] = "<"
+            else:
+                html_arr[i] = ""
+
+    return html_arr
+
+
+
+# Example usage
+html_content = """<html><head><title>My Title</title><meta charset="utf-8"></head><body><h1>Hello World!</h1><p>This is a paragraph.</p>hello</body></html>"""
+soup = BeautifulSoup(html_content, 'html.parser')
+
+head_tag = soup.head
+
+# k  = remove_content_tag(html_content)
+
+html_string = "<html>scsdasd</html>"
+
+# Use regular expression to find all HTML tags
+
+tokens = re.findall(r'<[^>]+>|[^<]+', html_content)
+
+print(remove_content(tokens))
+
+
+# def checking(arr):
+#     for i in arr:
+#         if (is_void_element(i)):
+#             print(i)
+        
+
+
+# checking(tokens)
+
